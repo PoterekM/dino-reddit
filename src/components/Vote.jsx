6a6302cbac9110bc3dacb0084c48c9aee9606c55
larry taylor and Michelle Poterek <michelle@>
2017-09-19 16:38:13 -0700

    try to get upvoting happening

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 
 function Vote(props){
   console.log(props);
+
+  function votePositive(postId){
+    const { dispatch } = props;
+    const { action } = {
+      type: c.UP_VOTE,
+      id: postId
+    }
+    dispatch(action)
+  }
   return (
     <div>
       <p>vote total: {props.voteTotal}</p>
-      <button onClick={() => {votePositive()}}>Vote Up</button>
+      <button onClick={() => {votePositive(props.post.id)}}>Vote Up</button>
       <button onClick={() => {console.log('goodbye')}}>Vote Down</button>
     </div>
   );
